5052c6f8b5b75a382926892fd07bec18b3f1463b
nikmat1996 <nikhilmathew1996@gmail.com>
2023-05-16 20:29:08 +0530

    start, rust is crazy

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::env::{args, Args};
+
+fn main() {
+
+    let mut args: Args = args();
+
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap();
+    let second = args.nth(0).unwrap();
+
+    println!("{} {} {}", first, operator, second);
+}
