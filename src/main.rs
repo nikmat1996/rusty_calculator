9bcef449283fbcc1d58c8c30d0983a41468d13a8
nikmat1996 <nikhilmathew1996@gmail.com>
2023-05-16 22:00:58 +0530

    rustified the last crazy

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let operator = args.nth(0).unwrap().chars().next().unwrap();
     let second = args.nth(0).unwrap();
 
-    let first_num: f32 = first.parse().unwrap();
+    let first_num = first.parse::<f32>().unwrap();
     let second_num = second.parse::<f32>().unwrap();
 
     println!("{} {} {}", first_num, operator, second_num);
@@ -17,18 +17,18 @@ fn main() {
 
     println!("answer = {}", answer);
 
-    let answerStr = output(first_num, operator, second_num, answer);
-    println!("{:?}", answerStr)
+    let answer_str = output(first_num, operator, second_num, answer);
+    println!("{:?}", answer_str)
 }
 
 fn operate(operator: char, first_num: f32, second_num: f32) -> f32 {
-    if operator == '+' {
-        return first_num + second_num;
-    } else if operator == '-' {
-        return first_num - second_num;
-    } else if operator == '*' {
-        return first_num * second_num;
-    } else { return first_num / second_num };
+    match operator {
+        '+' => first_num + second_num,
+        '-' => first_num - second_num,
+        '/' => first_num / second_num,
+        '*' => first_num * second_num,
+        _ => panic!("invalid operator")
+    }
 }
 
 fn output(first_num: f32, operator: char, second_num: f32, answer: f32) -> String {
